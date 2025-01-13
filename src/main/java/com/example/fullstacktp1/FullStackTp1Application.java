93360e62193c8b2bd1bc295535aacf20616ec744
smyaj <smyaj.abdelhakim@gmail.com>
2025-01-13 11:59:02 +0100

    first commit

---
+++ src/main/java/com/example/fullstacktp1/FullStackTp1Application.java
@@ -0,0 +1,13 @@
+package com.example.fullstacktp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackTp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackTp1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstacktp1/FullStackTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstacktp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackTp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
